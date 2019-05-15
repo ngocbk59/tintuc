306d9f7d9f9d4053b751b07ebebca6d37256fb48
HỒNG NGỌC ĐINH <ngocbk59@gmail.com>
2019-05-15 17:31:45 +0700

    tao trang sua va xoa slide

--- app/Http/Controllers/SlideController.php
+++ app/Http/Controllers/SlideController.php
@@ -49,12 +49,45 @@ class SlideController extends Controller
     	return redirect('admin/slide/them')->with('thongbao','Thêm thành công');
     }
     public function getSua($id){
-    	
+    	$slide = Slide::find($id);
+    	return view('admin.slide.sua',['slide'=>$slide]);
     }
     public function postSua(Request $request, $id){
+    	$this->validate($request,[
+    		'Ten'=>'required',
+    		'NoiDung'=>'required',
+    	],[
+    		'Ten.required'=>'Bạn chưa nhập tên',
+    		'NoiDung.required'=>'Bạn chưa nhập nội dung',
+    	]);
+    	$slide = Slide::find($id);
+    	$slide->Ten = $request->Ten;
+    	$slide->NoiDung = $request->NoiDung;
+    	if ($request->has('link')) {
+    		$slide->link = $request->link;
+    	}
     	
+    	if ($request->hasFile('Hinh')) {
+    		$file = $request->file('Hinh');
+    		$duoi = $file->getClientOriginalExtension();
+    		if ($duoi != 'jpg' && $duoi != 'png' && $duoi != 'jpeg') {
+    			return redirect('admin/slide/them')->with('loi','Bạn chỉ được chọn file có đuôi jpg, png, jpeg');
+    		}
+    		$name = $file->getClientOriginalName();
+    		$hinh = str_random(4) ."_". $name;
+    		while (file_exists("upload/slide/".$hinh)) {
+    			$hinh = str_random(4) ."_". $name;
+    		}
+    		unlink("upload/slide/".$slide->Hinh);
+    		$file->move("upload/slide",$hinh);
+    		$slide->Hinh = $hinh;
+    	}
+    	$slide->save();
+    	return redirect('admin/slide/sua/'.$id)->with('thongbao','Sửa thành công');
     } 
     public function getXoa($id){
-    	
+    	$slide = Slide::find($id);
+    	$slide->delete();
+    	return redirect('admin/slide/danhsach')->with('thongbao', 'Xóa thành công');
     }
 }
--- resources/views/admin/slide/sua.blade.php
+++ resources/views/admin/slide/sua.blade.php
@@ -5,47 +5,45 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Category
-                    <small>Edit</small>
+                <h1 class="page-header">Slide
+                    <small>{{$slide->Ten}}</small>
                 </h1>
             </div>
             <!-- /.col-lg-12 -->
             <div class="col-lg-7" style="padding-bottom:120px">
-                <form action="" method="POST">
-                    <div class="form-group">
-                        <label>Category Parent</label>
-                        <select class="form-control">
-                            <option value="0">Please Choose Category</option>
-                            <option value="">Tin Tức</option>
-                        </select>
+                @if(count($errors) > 0)
+                    <div class="alert-danger">
+                        @foreach($errors->all() as $err)
+                            {{$err}}<br>
+                        @endforeach
                     </div>
-                    <div class="form-group">
-                        <label>Category Name</label>
-                        <input class="form-control" name="txtCateName" placeholder="Please Enter Category Name" />
+                @endif
+                @if(session('thongbao'))
+                    <div class="alert alert-success">
+                        {{session('thongbao')}}
                     </div>
+                @endif
+                <form action="admin/slide/sua/{{$slide->id}}" method="POST" enctype="multipart/form-data">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">                   
                     <div class="form-group">
-                        <label>Category Order</label>
-                        <input class="form-control" name="txtOrder" placeholder="Please Enter Category Order" />
-                    </div>
+                        <label>Tên</label>
+                        <input class="form-control" name="Ten" placeholder="Nhập tên slide" value="{{$slide->Ten}}" />
+                    </div>                  
                     <div class="form-group">
-                        <label>Category Keywords</label>
-                        <input class="form-control" name="txtOrder" placeholder="Please Enter Category Keywords" />
-                    </div>
+                        <label>Nội dung</label>
+                        <textarea name="NoiDung" id="demo" class="ckeditor" rows="3">{{$slide->Ten}}</textarea>
+                    </div>    
                     <div class="form-group">
-                        <label>Category Description</label>
-                        <textarea class="form-control" rows="3"></textarea>
-                    </div>
+                        <label>Link</label>
+                        <input class="form-control" name="link" placeholder="Nhập link" value="{{$slide->link}}" />
+                    </div>               
                     <div class="form-group">
-                        <label>Category Status</label>
-                        <label class="radio-inline">
-                            <input name="rdoStatus" value="1" checked="" type="radio">Visible
-                        </label>
-                        <label class="radio-inline">
-                            <input name="rdoStatus" value="2" type="radio">Invisible
-                        </label>
-                    </div>
-                    <button type="submit" class="btn btn-default">Category Edit</button>
-                    <button type="reset" class="btn btn-default">Reset</button>
+                        <label>Hình ảnh</label>
+                        <p><img src="upload/slide/{{$slide->Hinh}}"></p>
+                        <input class="form-control" type="file" name="Hinh">
+                    </div>                  
+                    <button type="submit" class="btn btn-default">Sửa</button>
+                    <button type="reset" class="btn btn-default">Làm mới</button>
                 <form>
             </div>
         </div>
