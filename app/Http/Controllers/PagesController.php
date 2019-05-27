94ed5df18bdb8c9ddd275f37c5beb2700ae542bf
HỒNG NGỌC ĐINH <ngocbk59@gmail.com>
2019-05-27 15:45:03 +0700

    đổ dữ liệu ra menu

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\TheLoai;
+
+class PagesController extends Controller
+{
+    function trangchu(){
+    	$theloai = TheLoai::all();
+    	return view('pages.trangchu',['theloai'=>$theloai]);
+    }
+}
